2c53fdb26eac23cf235c04b75648a2e910e84e30
weiman <weiman188@163.com>
2020-10-16 15:24:30 +0800

    NSOperation的其他操作，例如监听完成、最大并发数等

--- Multithreading/Multithreading/Controllers/OperationOtherController.m
+++ Multithreading/Multithreading/Controllers/OperationOtherController.m
@@ -40,12 +40,61 @@
     [queue addOperation:op2];
 }
 
+//NSOperation执行完成
 - (IBAction)test2:(id)sender {
-    
+    NSOperationQueue * queue = [[NSOperationQueue alloc] init];
+    NSBlockOperation * op1 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"任务一，%@",[NSThread currentThread]);
+    }];
+    [op1 addExecutionBlock:^{
+        [NSThread sleepForTimeInterval:2.0];
+        NSLog(@"任务二，%@",[NSThread currentThread]);
+    }];
+    [op1 addExecutionBlock:^{
+        [NSThread sleepForTimeInterval:1.0];
+        NSLog(@"任务三，%@",[NSThread currentThread]);
+    }];
+    [op1 addExecutionBlock:^{
+        NSLog(@"任务四，%@",[NSThread currentThread]);
+    }];
+    op1.completionBlock = ^{
+        NSLog(@"任务都执行完成啦");
+    };
+    [queue addOperation:op1];
 }
-
+//maxConcurrentOperationCount 最大并发数
 - (IBAction)test3:(id)sender {
+    //1. 串行
+    NSOperationQueue * queue = [[NSOperationQueue alloc] init];
+    queue.maxConcurrentOperationCount = 1;
+    NSBlockOperation * op1 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"任务一，%@",[NSThread currentThread]);
+    }];
+    NSBlockOperation * op2 = [NSBlockOperation blockOperationWithBlock:^{
+        [NSThread sleepForTimeInterval:2.0];
+        NSLog(@"任务二，%@",[NSThread currentThread]);
+    }];
+    NSBlockOperation * op3 = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"任务三，%@",[NSThread currentThread]);
+    }];
+    [queue addOperation:op1];
+    [queue addOperation:op2];
+    [queue addOperation:op3];
     
+    //2. 一个操作的多个任务，设置最大并发数为1是没有效果的
+//    NSOperationQueue * queue2 = [[NSOperationQueue alloc] init];
+//    queue2.maxConcurrentOperationCount = 1;
+//    NSBlockOperation * op4 = [NSBlockOperation blockOperationWithBlock:^{
+//        NSLog(@"1，%@",[NSThread currentThread]);
+//    }];
+//    [op4 addExecutionBlock:^{
+//        [NSThread sleepForTimeInterval:2.0];
+//        NSLog(@"2，%@",[NSThread currentThread]);
+//    }];
+//    [op4 addExecutionBlock:^{
+//        NSLog(@"3，%@",[NSThread currentThread]);
+//    }];
+//    [queue2 addOperation:op4];
 }
 
 - (IBAction)test4:(id)sender {
